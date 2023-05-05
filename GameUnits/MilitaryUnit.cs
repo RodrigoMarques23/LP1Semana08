252b55956b06ef987020feb1b476e2a5adcb2f69
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-05-05 01:04:43 +0100

    Ex06 Solved

--- GameUnits/MilitaryUnit.cs
+++ GameUnits/MilitaryUnit.cs
@@ -8,11 +8,32 @@ namespace GameUnits
         public int XP { get; private set; }
         public override int Health { get => base.Health + XP; set => base.Health = value; }
         public override float Cost { get => AttackPower + XP; }
-        public MilitaryUnit() : base(5, 3) { }
+        public MilitaryUnit(int movement, int health, int attackPower)
+        : base(movement, health)
+        {
+            AttackPower = attackPower;
+            XP = 0;
+        }
+
+        /// <summary>
+        /// Receives a unit
+        /// Implement XP
+        /// Decrement health from the attacked unit
+        /// </summary>
+        /// <param name="u"></param>
         public void Attack(Unit u)
         {
             XP++;
             u.Health--;
+        }
+
+        /// <summary>
+        /// Converts de object to a string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return base.ToString() + String.Format(" AP={0,1} XP={1,1}", AttackPower, XP);
         }
     }
 }
--- GameUnits/Unit.cs
+++ GameUnits/Unit.cs
@@ -12,9 +12,23 @@ namespace GameUnits
             this.movement = movement;
             Health = health;
         }
+
+        /// <summary>
+        /// Prints how many slots the unit moves
+        /// </summary>
         public void Move()
         {
             Console.WriteLine($"You moved {movement} slots");
+        }
+
+        /// <summary>
+        /// Converts de object to a string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return String.Format(
+                "{0}:  HP={1:d} COST={2:f0}", this.GetType().Name, Health, Cost);
         }
     }
 }
