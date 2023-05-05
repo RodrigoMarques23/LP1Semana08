83f251b901bd31950068617d29986656204c6d6d
Butchsss <rodrigomarques23work@gmail.com>
2023-05-05 13:52:52 +0100

    DiplomatUnit Core ADD

--- GameUnits/DiplomatUnit.cs
+++ GameUnits/DiplomatUnit.cs
@@ -5,8 +5,28 @@ using System.Threading.Tasks;
 
 namespace GameUnits
 {
-    public class DiplomatUnit
+    public class DiplomatUnit : XPUnit
     {
-        
+        public override float Cost { get => 7.1f; }
+        public DiplomatUnit(int movement, int health, int xp) : base(6, 1, 0)
+        {
+
+        }
+        public void MakeAgreement(Unit unit)
+        {
+            if (unit.GetType == DiplomatUnit)
+            {
+                XP += 3;
+            }
+            if (unit.GetType == SettlerUnit)
+            {
+                XP += 2;
+            }
+            else
+            {
+                XP += 1;
+            }
+
+        }
     }
 }
